ba2a5259405ac745087282f16adfe3e5cb040f1d
Ayush Shakya <ayushakya2000@gmail.com>
2019-06-25 05:42:35 +0530

    Login Frontend Developed

--- soc-web/src/App.vue
+++ soc-web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-parallax src="https://firebasestorage.googleapis.com/v0/b/new111-87012.appspot.com/o/berries-blueberries-close-up-1153655.jpg?alt=media&token=bacbbc06-0187-48f8-8e08-7443e80dc8b3" height="100%">
+  <v-parallax src="https://firebasestorage.googleapis.com/v0/b/new111-87012.appspot.com/o/camera-coffee-colorful-1391374.jpg?alt=media&token=e3a92fe8-3755-461b-b500-598f76f56317" height="100%">
   <v-app style = "background: rgb(0,0,0,0)">
     <v-toolbar app color="rgb(0,0,0,0.5)">
       <v-toolbar-title class="headline text-uppercase" dark>
@@ -25,100 +25,97 @@
       </template>
       </v-btn>
     </v-toolbar>
-
-    <v-layout align-end justify-center>
-    <v-flex>
-    <v-card color="rgb(0,0,0,0)" flat>
-      <v-container fluid grid-list-md>
-
-        <v-layout align-center justify-center row wrap>
-
-          <v-flex xs12 sm6 md5 lg4 ma-3 mt-5 >
-          <v-hover>
-            <v-card
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 18 : 2}`"
-                class="mx-auto"
-                ripple
-              >
-              <v-img
-                class="white--text"
-                height="50vh"
-                src="https://firebasestorage.googleapis.com/v0/b/new111-87012.appspot.com/o/adult-business-people-businessmen-1059116.jpg?alt=media&token=3c850adb-7864-4b65-9f37-f1e3f673ac00"
-              >
-                <v-container fill-height fluid>
-                  <v-layout align-end fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline black--text">Forums</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-            </v-card>
-          </v-hover>
-          </v-flex>
-
-          <v-flex xs12 sm6 md5 lg4 ma-3 mt-5>
-
-          <v-hover>
-            <v-card
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 18 : 2}`"
-                class="mx-auto"
-                ripple
-              >
-              <v-img
-                class="white--text"
-                height="50vh"
-                src="https://firebasestorage.googleapis.com/v0/b/new111-87012.appspot.com/o/administration-architecture-building-256490.jpg?alt=media&token=b3f3978d-8acf-4bd0-9453-ca67a24c572f"
-              >
-                <v-container fill-height fluid>
-                  <v-layout align-end fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline">Colleges</span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-            </v-card>
-          </v-hover>
-          </v-flex>
+    <v-layout align-center justify-center>
+    <v-flex xs12 sm6 md5 lg4>
+      <v-card color="rgb(0,0,0,0.5)">
+        <br>
+        <v-card-title>
+          <v-layout row wrap align-center justify-center>
+            <span class = "grey--text display-1 display-1 lighten-4">GET</span>
+            <span class = "white--text display-1 font-weight-light">Connected</span>
+          </v-layout>
+        </v-card-title>
+        
+        <v-layout justify-center>
+          <v-card-actions>
+            <v-btn @click="signupGoogle" color="#3b5998" dark large><v-icon small left>fab fa-facebook-f</v-icon>Facebook</v-btn>
+            
+            <v-btn @click="signupFacebook" color="#db4a39" dark large><v-icon small left>fab fa-google</v-icon>Google</v-btn>
+          </v-card-actions>
         </v-layout>
+        <br>
+          <v-layout row wrap align-center justify-center>
+            <span text-decor class = "grey--text body-1 font-weight-light lighten-4"><v-divider></v-divider><i>OR</i><v-divider></v-divider></span>
+          </v-layout>
+        
+         <v-form>
+          <v-container>
+            <v-layout row wrap justify-center>
 
-      </v-container>
-    </v-card>
+              <v-flex xs12 sm4>
+                <v-text-field
+                  box
+                  dark
+                  :rules="[rules.required, rules.counter]"
+                  label="First Name"
+                  counter
+                  maxlength="20"
+                  clearable
+                ></v-text-field>
+              </v-flex>
+
+              <v-flex xs12 sm4>
+                <v-text-field
+                  box
+                  dark
+                  :rules="[rules.required, rules.counter]"
+                  label="Last Name"
+                  counter
+                  maxlength="20"
+                  clearable
+                ></v-text-field>
+              </v-flex>
+
+              <v-flex sm8>
+                <v-text-field
+                  box
+                  dark
+                  v-model="email"
+                  :rules="[rules.required, rules.email]"
+                  label="E-mail"
+                  clearable
+                ></v-text-field>
+              </v-flex>
+
+              <v-flex sm8>
+              <v-text-field
+                v-model="password"
+                :append-icon="show1 ? 'visibility' : 'visibility_off'"
+                :rules="[rules.required, rules.min]"
+                :type="show1 ? 'text' : 'password'"
+                box
+                dark
+                name="input-10-1"
+                label="Password"
+                hint="At least 8 characters"
+                counter
+                clearable
+                @click:append="show1 = !show1"
+              ></v-text-field>
+            </v-flex>
+
+            </v-layout>
+          </v-container>
+        </v-form>
+
+      </v-card>
+
     </v-flex>
-    </v-layout>
-
+  </v-layout>
   </v-app>
   </v-parallax>
 </template>
 
-<script>
-  export default {
-    data () {
-      return {
-        loader: null,
-        loading: false,
-        loading2: false,
-        loading3: false,
-        loading4: false
-      }
-    },
-    watch: {
-      loader () {
-        const l = this.loader
-        this[l] = !this[l]
-
-        setTimeout(() => (this[l] = false), 3000)
-
-        this.loader = null
-      }
-    }
-
-  }
-
-</script>
 
 <style>
   .custom-loader {
@@ -158,3 +155,44 @@
     }
   }
 </style>
+
+<script>
+  
+  export default {
+    data () {
+      return {
+        loader: null,
+        loading: false,
+        loading2: false,
+        loading3: false,
+        loading4: false,
+        title: '',
+        email: '',
+        rules: {
+          required: value => !!value || 'Required.',
+          counter: value => value.length <= 20 || 'Max 20 characters',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          email: value => {
+            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return pattern.test(value) || 'Invalid e-mail.'
+          },
+          show1: false,
+            show2: true,
+            show3: false,
+            show4: false,
+            password: 'Password'
+          }
+        }
+    },
+    watch: {
+      loader () {
+        const l = this.loader
+        this[l] = !this[l]
+
+        setTimeout(() => (this[l] = false), 3000)
+
+        this.loader = null
+      }
+    }
+  }
+</script>
